33e5fcb5c2a531f49db431a624c87a1581810eb8
Brad <bradharris6885@gmail.com>
2017-10-10 13:54:38 -0400

    oct-10 updates

--- ncaa.js
+++ ncaa.js
@@ -1,10 +1,23 @@
 function showStandings(standings, element) {
     for(var row in standings) {
 	let line = standings[row];
-	let txt = line['owner'] + " (" + line['wins'] + ", " + line['losses'] + ")";
+
+	let owner = document.createElement('span');
+	owner.setAttribute('class', 'name');
+	owner.innerText = line.owner;
+
+	let wins = document.createElement('span');
+	wins.setAttribute('class', 'record');
+	wins.innerText = line.wins;
+
+	let losses = document.createElement('span');
+	losses.setAttribute('class', 'record');
+	losses.innerText = line.losses;
+
 	let item = document.createElement("li");
-	let textElement = document.createTextNode(txt);
-	item.appendChild(textElement);
+	item.appendChild(owner);
+	item.appendChild(wins);
+	item.appendChild(losses);
 	element.appendChild(item);
     }
 }
@@ -27,5 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     showDraft(window.FOOTBALL.ncaaDraft || {}, document.getElementById("ncaa-draft"));
     showStandings(window.FOOTBALL.nflStandings || {}, document.getElementById("nfl-standings"));
     showDraft(window.FOOTBALL.nflDraft || {}, document.getElementById("nfl-draft"));
+    showStandings(window.FOOTBALL.overallStandings || {}, document.getElementById("overall-standings"));
 
 }, false);
---
+++ overallJson.js
@@ -0,0 +1,3 @@
+window.FOOTBALL = window.FOOTBALL || {}
+
+window.FOOTBALL.overallStandings=[{"owner": "Brad", "wins": "60", "losses": "34"}, {"owner": "Brian", "wins": "59", "losses": "35"}, {"owner": "Chad", "wins": "59", "losses": "35"}, {"owner": "Mike", "wins": "57", "losses": "39"}, {"owner": "Adam", "wins": "49", "losses": "40"}]
