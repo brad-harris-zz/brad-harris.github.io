6b2328ea09513a8fd46c2d4f4f9de5e14a4d583a
Brad <bradharris6885@gmail.com>
2017-10-17 20:58:27 -0400

    Final week 7/6 updates

--- overallJson.js
+++ overallJson.js
@@ -1,3 +1,3 @@
 window.FOOTBALL = window.FOOTBALL || {}
 
-window.FOOTBALL.overallStandings=[{"owner": "Chad", "wins": "67", "losses": "40"}, {"owner": "Mike", "wins": "67", "losses": "45"}, {"owner": "Brian", "wins": "65", "losses": "41"}, {"owner": "Brad", "wins": "63", "losses": "43"}, {"owner": "Adam", "wins": "56", "losses": "49"}]
+window.FOOTBALL.overallStandings=[{"owner": "Mike", "wins": "69", "losses": "45"}, {"owner": "Brian", "wins": "67", "losses": "43"}, {"owner": "Chad", "wins": "67", "losses": "41"}, {"owner": "Brad", "wins": "66", "losses": "45"}, {"owner": "Adam", "wins": "57", "losses": "51"}]
