c3c0805f4607de05c38e5fd9550172af744b2ea8
Brad <bradharris6885@gmail.com>
2017-11-28 00:15:32 -0500

    Nov 27

--- overallJson.js
+++ overallJson.js
@@ -1,3 +1,3 @@
 window.FOOTBALL = window.FOOTBALL || {}
 
-window.FOOTBALL.overallStandings=[{"owner": "Brian", "wins": "113", "losses": "78"}, {"owner": "Mike", "wins": "110", "losses": "80"}, {"owner": "Chad", "wins": "109", "losses": "82"}, {"owner": "Brad", "wins": "109", "losses": "81"}, {"owner": "Adam", "wins": "99", "losses": "92"}]
+window.FOOTBALL.overallStandings=[{"owner": "Mike", "wins": "124", "losses": "86"}, {"owner": "Chad", "wins": "122", "losses": "88"}, {"owner": "Brian", "wins": "120", "losses": "89"}, {"owner": "Brad", "wins": "117", "losses": "91"}, {"owner": "Adam", "wins": "106", "losses": "103"}]
