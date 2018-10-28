86ed68d72eeb34305126a078fede911ed7d00915
Brad <bradharris6885@gmail.com>
2018-10-28 11:55:09 -0400

    Updating homepage

--- football/service-worker.js
+++ football/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdbd1f207280331ac4d99a79f48ce13d.js"
+  "/football/precache-manifest.1d5848d8f42a2977b0796679797e1073.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/football/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
