14b44b3ab82198816672dced56d72280267bbcb5
Brad <bradharris6885@gmail.com>
2018-10-28 11:50:53 -0400

    Removing old client

--- football/overallJson.js
+++
@@ -1,3 +0,0 @@
-window.FOOTBALL = window.FOOTBALL || {}
-
-window.FOOTBALL.overallStandings=[{"owner": "Mike", "wins": "124", "losses": "86"}, {"owner": "Chad", "wins": "122", "losses": "88"}, {"owner": "Brian", "wins": "120", "losses": "89"}, {"owner": "Brad", "wins": "117", "losses": "91"}, {"owner": "Adam", "wins": "106", "losses": "103"}]
