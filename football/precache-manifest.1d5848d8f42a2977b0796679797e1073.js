76053a4e548dcb52bac3679c7166c45c32e3b098
Brad <bradharris6885@gmail.com>
2018-10-28 11:55:09 -0400

    Updating homepage

---
+++ football/precache-manifest.1d5848d8f42a2977b0796679797e1073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5372b0af76b32cf76d3",
+    "url": "/football/static/js/runtime~main.c5372b0a.js"
+  },
+  {
+    "revision": "0d57034e988ab861c26a",
+    "url": "/football/static/js/main.0d57034e.chunk.js"
+  },
+  {
+    "revision": "0ae18135f81023f67c5d",
+    "url": "/football/static/js/1.0ae18135.chunk.js"
+  },
+  {
+    "revision": "0d57034e988ab861c26a",
+    "url": "/football/static/css/main.47358643.chunk.css"
+  },
+  {
+    "revision": "525ee2c33a840a5f99e83d4433cb2ef0",
+    "url": "/football/index.html"
+  }
+];
